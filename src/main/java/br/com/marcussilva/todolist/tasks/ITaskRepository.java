a3e5d3e3da92433d0d2250f70d9a78de9d27fac0
marcus.vinicius <marcus.vinicius@logiquesistemas.com.br>
2023-11-29 13:45:04 -0300

    ITaskRepository

---
+++ src/main/java/br/com/marcussilva/todolist/tasks/ITaskRepository.java
@@ -0,0 +1,6 @@
+package br.com.marcussilva.todolist.tasks;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {}
