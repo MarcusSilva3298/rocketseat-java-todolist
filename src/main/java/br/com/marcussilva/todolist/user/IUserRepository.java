3c63c8ec553a8858a5ab8ae11aeed57c982de50c
marcus.vinicius <marcus.vinicius@logiquesistemas.com.br>
2023-11-29 13:44:43 -0300

    prettier

--- src/main/java/br/com/marcussilva/todolist/user/IUserRepository.java
+++ src/main/java/br/com/marcussilva/todolist/user/IUserRepository.java
@@ -1,9 +1,7 @@
 package br.com.marcussilva.todolist.user;
 
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
   UserModel findByUsername(String username);
--- src/main/java/br/com/marcussilva/todolist/user/UserModel.java
+++ src/main/java/br/com/marcussilva/todolist/user/UserModel.java
@@ -1,15 +1,13 @@
 package br.com.marcussilva.todolist.user;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @Entity(name = "tb_users")
@@ -21,10 +19,10 @@ public class UserModel {
 
   @Column(unique = true)
   private String username;
+
   private String name;
   private String password;
 
   @CreationTimestamp
   private LocalDateTime createdAt;
 }
- 
